3e2dbe17dabb4d69afa7ee86d1c6b1e430a67218
Wiktor <wiktorjck@gmail.com>
2026-01-01 16:22:41 +0100

    feat(desktop): implement initial  layout and update header structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 let navBtn = document.querySelector(".header__hamburger");
 let nav = document.querySelector(".header__nav");
+let body = document.querySelector("body");
 function handleNav(){
     nav.classList.toggle("nav-expanded");
 }
